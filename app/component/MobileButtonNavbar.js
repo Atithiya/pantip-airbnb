97b957b61abf480c979894b1c33a6258e6a58ead
Atithiya <atithiya.inthorn@gmail.com>
2024-02-22 21:28:59 +0700

    Navbar:remove border-b of categories (mobile), MobileButtonNavbar:edit py

--- app/component/MobileButtonNavbar.js
+++ app/component/MobileButtonNavbar.js
@@ -1,7 +1,7 @@
 export function MobileButtonNavbar() {
   return (
-    <div className="lg:hidden px-6 py-3 ">
-      <div className="btn btn-ghost border-gray-300 flex gap-5 rounded-xl  group hover:cursor-pointer bg-white">
+    <div className="lg:hidden px-6 py-4 ">
+      <div className="btn btn-ghost border-gray-300 flex gap-5 rounded-xl group hover:cursor-pointer bg-white">
         <div className="font-semibold text-lg">แสดงกระทู้ที่ยังไม่อ่าน</div>
         <input type="checkbox" className="toggle toggle-sm" />
       </div>
--- app/component/Navbar.js
+++ app/component/Navbar.js
@@ -6,7 +6,7 @@ import { MobileButtonNavbar } from "./MobileButtonNavbar";
 export function Navbar() {
   return (
     <div className="fixed w-full shadow-sm top-0 bg-white z-10">
-      <div className="py-5 xl:px-10 md:px-10 px-6   mx-auto border-b-[1px] flex justify-between">
+      <div className="py-5 xl:px-10 md:px-10 px-6   mx-auto lg:border-b-[1px] flex justify-between">
         <Logo />
         <Search />
         <UserMenu />
